588b709733072a268bd2409d4f714a75090dcf6b
Amelia Nathasa <95131827+amelianathasa@users.noreply.github.com>
2023-09-12 21:54:15 +0700

    update

---
+++ specialmission/Nodejs-Reactjs-CRUD/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+var bodyParser = require("body-parser");
+
+global.__basedir = __dirname;
+
+const db = require("./app/config/db.config.js");
+
+const Customer = db.Customer;
+
+let router = require("./app/routers/router.js");
+
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:4200",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(express.static("resources"));
+app.use("/", router);
+
+// Create a Server
+const server = app.listen(8080, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log("App listening at http://%s:%s", host, port);
+});
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and Resync with { force: true }");
+  Customer.sync().then(() => {
+    const customers = [
+      { firstname: "Amelia", lastname: "Nathasa", age: 20, address: "Cimahi" },
+      { firstname: "Khany", lastname: "Nur", age: 20, address: "Bandung" },
+      { firstname: "Meira", lastname: "Nurul", age: 20, address: "Cimahi" },
+    ];
+
+    for (let i = 0; i < customers.length; i++) {
+      Customer.create(customers[i]);
+    }
+  });
+});
